b95ae4390389afb90e924b2098eaf51c989e88b1
malbuffer4pt <icbluewhale@gmail.com>
2026-04-27 16:08:04 +0800

    update

--- OpenCry/Source.cpp
+++ OpenCry/Source.cpp
@@ -2,10 +2,53 @@
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
+	switch (uMsg)
+	{
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	case WM_LBUTTONDOWN:
+		MessageBox(hwnd, TEXT("Hello, Win32"), TEXT("Clicked"), MB_OK);
+		return 0;
+	}
 
+	return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow)
 {
+	const wchar_t CLASS_NAME[] = TEXT("MyWindowClass");
 
+	WNDCLASS wc = {};
+	wc.lpfnWndProc = WindowProc;
+	wc.hInstance = hInstance;
+	wc.lpszClassName = CLASS_NAME;
+
+	RegisterClass(&wc);
+
+	HWND hwnd = CreateWindowEx(
+		0,
+		CLASS_NAME,
+		TEXT("Native C++ Window"),
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		500,
+		300,
+		NULL,
+		NULL,
+		hInstance,
+		NULL
+	);
+
+	ShowWindow(hwnd, nCmdShow);
+
+	MSG msg = {};
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+
+	return 0;
 }
